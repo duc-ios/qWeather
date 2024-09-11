79a63e8d557e53397117cdba75eb8a6422145ca0
Duc Nguyen <duc.nh2012@gmail.com>
2024-09-12 03:38:14 +0700

    refactor: Router

--- qWeather/Utils/Route.swift
+++ qWeather/Utils/Route.swift
@@ -5,9 +5,24 @@
 //  Created by Duc on 29/8/24.
 //
 
+import SwiftUI
+
 enum Route: Hashable {
     case landing,
          onboarding,
          home,
          detail(CityModel)
+
+    @ViewBuilder var destination: some View {
+        switch self {
+        case .landing:
+            LandingView().configured()
+        case .onboarding:
+            OnboardingView()
+        case .home:
+            HomeView().configured()
+        case .detail(let city):
+            DetailView().configured(city: city)
+        }
+    }
 }
